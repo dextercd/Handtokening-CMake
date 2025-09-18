fb1e98de6b85973b0a8004367bbf98457ee4e330
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2025-09-18 22:49:43 +0200

    Rework find_program cache handling

--- cmake/FindHT_CURL.cmake
+++ cmake/FindHT_CURL.cmake
@@ -1,7 +1,7 @@
-find_program(HT_CURL curl)
+find_program(HT_CURL_EXECUTABLE curl)
 
-if(HT_CURL)
-    execute_process(COMMAND ${HT_CURL} --version
+if(HT_CURL_EXECUTABLE)
+    execute_process(COMMAND ${HT_CURL_EXECUTABLE} --version
         OUTPUT_VARIABLE curl_version_output
         ERROR_VARIABLE curl_version_error
         RESULT_VARIABLE curl_version_result
@@ -9,22 +9,22 @@ if(HT_CURL)
     )
 
     if(NOT curl_version_result EQUAL 0)
-        message(SEND_ERROR "Command failed: ${HT_CURL} --version:\n${curl_version_error}")
+        message(SEND_ERROR "Command failed: ${HT_CURL_EXECUTABLE} --version:\n${curl_version_error}")
     else()
         if(curl_version_output MATCHES "^curl ([0-9]+[.][0-9]+[.][0-9]+)")
             set(HT_CURL_VERSION "${CMAKE_MATCH_1}")
         else()
-            message(SEND_ERROR "Couldn't get version number from ${HT_CURL} --version:\n${curl_version_output}")
+            message(SEND_ERROR "Couldn't get version number from ${HT_CURL_EXECUTABLE} --version:\n${curl_version_output}")
         endif()
     endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(HT_CURL
-    REQUIRED_VARS HT_CURL HT_CURL_VERSION
+    REQUIRED_VARS HT_CURL_EXECUTABLE HT_CURL_VERSION
     VERSION_VAR HT_CURL_VERSION
 )
 
-if(HT_CURL AND NOT HT_CURL_FOUND)
-    set(HT_CURL 0 CACHE STRING "" FORCE)
+if(HT_CURL_FOUND)
+    set(HT_CURL "${HT_CURL_EXECUTABLE}")
 endif()
--- cmake/FindHT_OSSLSIGNCODE.cmake
+++ cmake/FindHT_OSSLSIGNCODE.cmake
@@ -1,7 +1,7 @@
-find_program(HT_OSSLSIGNCODE osslsigncode)
+find_program(HT_OSSLSIGNCODE_EXECUTABLE osslsigncode)
 
-if(HT_OSSLSIGNCODE)
-    execute_process(COMMAND ${HT_OSSLSIGNCODE} --version
+if(HT_OSSLSIGNCODE_EXECUTABLE)
+    execute_process(COMMAND ${HT_OSSLSIGNCODE_EXECUTABLE} --version
         OUTPUT_VARIABLE osslsigncode_version_output
         ERROR_VARIABLE osslsigncode_version_error
         RESULT_VARIABLE osslsigncode_version_result
@@ -9,22 +9,22 @@ if(HT_OSSLSIGNCODE)
     )
 
     if(NOT osslsigncode_version_result EQUAL 0)
-        message(SEND_ERROR "Command failed: ${HT_OSSLSIGNCODE} --version:\n${osslsigncode_version_error}")
+        message(SEND_ERROR "Command failed: ${HT_OSSLSIGNCODE_EXECUTABLE} --version:\n${osslsigncode_version_error}")
     else()
         if(osslsigncode_version_output MATCHES "^osslsigncode ([0-9]+[.][0-9]+([.][0-9]+)?)")
             set(HT_OSSLSIGNCODE_VERSION "${CMAKE_MATCH_1}")
         else()
-            message(SEND_ERROR "Couldn't get version number from ${HT_OSSLSIGNCODE} --version:\n${osslsigncode_version_output}")
+            message(SEND_ERROR "Couldn't get version number from ${HT_OSSLSIGNCODE_EXECUTABLE} --version:\n${osslsigncode_version_output}")
         endif()
     endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(HT_OSSLSIGNCODE
-    REQUIRED_VARS HT_OSSLSIGNCODE HT_OSSLSIGNCODE_VERSION
+    REQUIRED_VARS HT_OSSLSIGNCODE_EXECUTABLE HT_OSSLSIGNCODE_VERSION
     VERSION_VAR HT_OSSLSIGNCODE_VERSION
 )
 
-if(HT_OSSLSIGNCODE AND NOT HT_OSSLSIGNCODE_FOUND)
-    set(HT_OSSLSIGNCODE 0 CACHE STRING "" FORCE)
+if(HT_OSSLSIGNCODE_FOUND)
+    set(HT_OSSLSIGNCODE "${HT_OSSLSIGNCODE_EXECUTABLE}")
 endif()
